c47367c8e8e7aa74eb05de5e7ea99277d4bab058
pe-hy <petr.hyner10@volny.cz>
2024-01-21 19:52:29 +0100

    initial stuff

--- custom/0_stuff.py
+++ custom/0_stuff.py
@@ -11,6 +11,7 @@ from tqdm.auto import trange, tqdm
 
 from xminigrid.wrappers import GymAutoResetWrapper
 
+
 # alternatively users can provide step_fn and reset_fn instead
 # of the closure, but in whis way it is simpler to use after the creation
 def build_rollout(env, env_params, num_steps):
@@ -18,15 +19,19 @@ def build_rollout(env, env_params, num_steps):
         def _step_fn(carry, _):
             rng, timestep = carry
             rng, _rng = jax.random.split(rng)
-            action = jax.random.randint(_rng, shape=(), minval=0, maxval=env.num_actions(env_params))
-            
+            action = jax.random.randint(
+                _rng, shape=(), minval=0, maxval=env.num_actions(env_params)
+            )
+
             timestep = env.step(env_params, timestep, action)
             return (rng, timestep), timestep
-    
+
         rng, _rng = jax.random.split(rng)
-    
+
         timestep = env.reset(env_params, _rng)
-        rng, transitions = jax.lax.scan(_step_fn, (rng, timestep), None, length=num_steps)
+        rng, transitions = jax.lax.scan(
+            _step_fn, (rng, timestep), None, length=num_steps
+        )
         return transitions
 
     return rollout
@@ -36,18 +41,23 @@ env, env_params = xminigrid.make("MiniGrid-EmptyRandom-8x8")
 # do not forget to use auto reset wrapper!
 env = GymAutoResetWrapper(env)
 
-vmap_rollout = jax.jit(jax.vmap(build_rollout(env, env_params, num_steps=1000)))
-rngs = jax.random.split(jax.random.PRNGKey(0), num=1024)
+# jiting the entire rollout
+rollout_fn = jax.jit(build_rollout(env, env_params, num_steps=1000))
 
-vmap_transitions = vmap_rollout(rngs)
+# first execution will compile
+transitions = rollout_fn(jax.random.PRNGKey(0))
 
-print(f"Transitions shapes: \n, {jtu.tree_map(jnp.shape, vmap_transitions)}")
+# vmap_rollout = jax.jit(jax.vmap(build_rollout(env, env_params, num_steps=1000)))
+# rngs = jax.random.split(jax.random.PRNGKey(0), num=1024)
+# vmap_transitions = vmap_rollout(rngs)
+
+print(f"Transitions shapes: \n, {jtu.tree_map(jnp.shape, transitions)}")
 
 # optionally render the state
 images = []
 
 for i in trange(1000):
-    timestep = jtu.tree_map(lambda x: x[i], vmap_transitions)
+    timestep = jtu.tree_map(lambda x: x[i], transitions)
     images.append(env.render(env_params, timestep))
 
-imageio.mimsave("custom/example_rollout.mp4", images, fps=32, format="mp4")
+imageio.mimsave("example_rollout.mp4", images, fps=32, format="mp4")
